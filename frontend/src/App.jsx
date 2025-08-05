070230b7a5c76a464ff4c9181f656e5198c0dd6f
pulkit1057 <100367696+pulkit1057@users.noreply.github.com>
2025-08-05 20:47:53 +0530

    Added navbar to the UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Routes , Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import PlaceOrder from './pages/PlaceOrder'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/product/:productID' element={<Product/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/place-order' element={<PlaceOrder/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/orders' element={<Orders/>} />
+      </Routes>
       
     </div>
   )
